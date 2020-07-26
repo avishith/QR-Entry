12c9de4e0a8ce9937b5ecd7d3858fdbe01e28bf3
Hari <baluoger91@gmail.com>
2020-07-26 16:59:32 +0530

    QR read and display contents...Need more updates for Security system

--- qrcode.py
+++ qrcode.py
@@ -2,8 +2,21 @@ import numpy as np
 from pyzbar.pyzbar import decode
 import cv2
 
-img=cv2.imread('code.png')
-code=decode(img)
-for qrCode in code:
-    mytext=qrCode.data
-    print(mytext)
+cap=cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
+while True:
+    succes,img=cap.read()
+    code=decode(img)
+    for qr in code:
+        val=qr.data.decode('utf-8')
+        points=np.array([qr.polygon],np.int32)
+        cv2.polylines(img,[points],True,(0,255,0),3)
+        rec=qr.rect
+        cv2.putText(img,val,(rec[0],rec[1]),cv2.FONT_HERSHEY_PLAIN,1,(255,0,0),1)
+        print(val)
+    cv2.imshow('QR-Enrty',img)
+    cv2.waitKey(2)
+
+#img=cv2.imread('code1.png')
+
